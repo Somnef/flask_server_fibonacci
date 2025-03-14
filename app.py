b1bdf9422d63dfe0fbb5b0c2c0909ba938042ce9
Bousselat Ahmed Moncef <a.m.bousselat@gmail.com>
2025-03-14 22:08:06 +0100

    added identification to app

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import flask
+import random
 
 app = flask.Flask(__name__)
+random_id = random.randint(0, 1000)
 
 # main route returns fibonacci number
 @app.route('/<int:n>')
 def fibonacci(n):
-    return "Fibonacci number is: " + str(fib(n)) + "\n"
+    return "Computer <" + str(random_id) + "> says Fibonacci number is: " + str(fib(n)) + "\n"
 
 # fibonacci function
 def fib(n):
